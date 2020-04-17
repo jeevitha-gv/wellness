3be2295fd8c45ed5ae6772e1d13cc6167ae5122e
ROBIN-5894 <robin@fixnix.co>
2020-04-17 16:35:05 +0530

    Updated the Footer Message

--- footer.php
+++ footer.php
@@ -108,7 +108,7 @@
             <img alt="Logo" src="./assets/media/logos/logo-2-sm.png">
           </a>    -->        
           <div class="kt-footer__copyright" style="color: white;font-size:1.1rem">
-          The CoronaSpeak  is a platform which aims to share information about public health events and emergencies. <a style="color: white;" onclick="document.getElementById('id03').style.display='block'">Click More</a>
+          The WellnessSpeak  is a platform which aims to record your Emotional Wellness state and display results based on the answer to the questionnaire. <a style="color: white;" onclick="document.getElementById('id03').style.display='block'">Click More</a>
 
           </div>
           
@@ -153,23 +153,11 @@
     </div>
 
     <div style="line-height:300%;color:#525150;border-color:#B4B8B8;padding:26px;">
-     <h3>CoronaSpeak Disclaimer</h3><br>
+     <h3>Wellness Speak Disclaimer</h3><br>
 
-The CoronaSpeak  is a platform which aims to share information about public health events and emergencies.
-
-The data on the dashboard is refreshed every fifteen (15) minutes and data is accurate as at time of refreshing. We source info from WHO and many other sources.
-
-CoronaSpeak is just an initiatve to support the citizens who want to submit cases, info to authorities like WHO, governments anonymously.
-The CoronaSpeak  is not a comprehensive representation of all the events and emergencies that CoronaSpeak is aware of and responding to. The content of the CoronaSpeak  is for general information only.<span id="dots">....</span><span id="more">
-It is subject to change without notice. While every reasonable effort has been made to make the information on the CoronaSpeak  as timely and accurate as possible, CoronaSpeak makes no claims, promises or guarantees about the effectiveness, completeness and accuracy of the contents of the CoronaSpeak , and expressly disclaims any liability for damages as a result of the use and/or application of the CoronaSpeak , errors and/or omissions in the content.
-
-The responsibility for the interpretation and use of the content lies with the reader. CoronaSpeak reserves the right to make updates and changes to posted content without notice and accepts no liability for any errors or omissions in this regard.
-
-CoronaSpeak assumes no responsibility or liability for any consequence resulting directly or indirectly from any action or inaction readers take based on or made in reliance on the information and material available on the CoronaSpeak .
-
-While every reasonable effort has been made to use appropriate language and pictures on the CoronaSpeak , CoronaSpeak expressly disclaims any responsibility for inadvertent offensive or insensitive, perceived or actual, language or pictures. CoronaSpeak will take no responsibility for or be liable for the CoronaSpeak  being temporarily unavailable in the event of technical or other issues.
-
-The size of the pie charts corresponds to the number of events in a given country or territory; the size does not indicate the severity or risk associated with the event(s). The designations employed and the presentation of content on the CoronaSpeak , including names of the events, maps and other illustrative materials, do not imply the expression of any opinion whatsoever on the part of CoronaSpeak concerning the legal status of any country, territory, city or area, or of its authorities, or concerning the delineation of frontiers and borders. Grey areas on maps represent approximate border lines for which there may not yet be full agreement.</span>
+We are here to help. 
+First off, this is a secure consulting room and your identity is protected. We use the latest technology of Block Chain to provide this completely anonymous way of communicating. <span id="dots">....</span><span id="more">
+Neither your name, nor your telephone number is captured unless you voluntarily provide it. Please save the Speak number and key and should you need to visit us again, you may use them to log in to view your previous report with us.  There will be no trace of your contact with us other than this speak number. Should you choose to use the “Chat to a Wellness Expert” facility, it will connect to our telegram group from where a realtime counsellor team  will be available on call.  How may we help you?</span>
 <span onclick="myFunction()" id="myBtn" style="color:#034ea2;">Read more</span>
      
     </div>
