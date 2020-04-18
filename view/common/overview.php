d1393158831ab08033f9ad4de991404e8cfe2595
ROBIN-5894 <robin@fixnix.co>
2020-04-18 10:03:57 +0530

    Title updatedd

--- view/common/overview.php
+++ view/common/overview.php
@@ -59,7 +59,7 @@ $companyId=$id[0]['id'];
 <base href="/wellness/"><!--end::Base Path -->
         <meta charset="utf-8"/>
 
-        <title>coronaspeak-NixWhistle</title>
+        <title>Emotional Wellness -Dashboard</title>
         <meta name="description" content="Add user example">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
