c29513537cb144ea2242bd7a567b31262f2751bb
ROBIN-5894 <robin@fixnix.co>
2020-04-19 01:56:10 +0530

    Hey

---
+++ assets/js/demo2/scriptresult.js
@@ -0,0 +1,534 @@
+  function mum(){
+  numb();
+  num();
+}
+  function num(){
+  var test = document.querySelector('input[name=quitetrivial]:checked').value;
+  document.getElementById('res1').innerHTML = test;
+  var age = document.querySelector('input[name=dryness]:checked').value;
+  document.getElementById('res2').innerHTML = age;
+  var gender = document.querySelector('input[name=positivefeeling]:checked').value;
+  document.getElementById('res3').innerHTML = gender;
+  var breathing = document.querySelector('input[name=breathing]:checked').value;
+  document.getElementById('res4').innerHTML = breathing;
+  var fever = document.querySelector('input[name=getgoing]:checked').value;
+  document.getElementById('res5').innerHTML = fever;
+  var drycough = document.querySelector('input[name=situations]:checked').value;
+  document.getElementById('res6').innerHTML = drycough;
+  var feelingsmell = document.querySelector('input[name=shakiness]:checked').value;
+  document.getElementById('res7').innerHTML = feelingsmell;
+  var throatpain = document.querySelector('input[name=relax]:checked').value;
+  document.getElementById('res8').innerHTML = throatpain;
+  var bodyweak = document.querySelector('input[name=anxious]:checked').value;
+  document.getElementById('res9').innerHTML = bodyweak;
+  var nohunger = document.querySelector('input[name=forward]:checked').value;
+  document.getElementById('res10').innerHTML = nohunger;
+  var coughrange = document.querySelector('input[name=upset]:checked').value;
+  document.getElementById('res11').innerHTML = coughrange;
+  var shortness = document.querySelector('input[name=nervous]:checked').value;
+  document.getElementById('res12').innerHTML = shortness;
+  var drowsy = document.querySelector('input[name=saddepressed]:checked').value;
+  document.getElementById('res13').innerHTML = drowsy;
+  var chestpain = document.querySelector('input[name=impatient]:checked').value;
+  document.getElementById('res14').innerHTML = chestpain;
+  var weaknessbody = document.querySelector('input[name=faintness]:checked').value;
+  document.getElementById('res15').innerHTML = weaknessbody;
+  var visitedoutside = document.querySelector('input[name=lostinterest]:checked').value;
+  document.getElementById('res16').innerHTML = visitedoutside;
+  var contactaffect = document.querySelector('input[name=worthperson]:checked').value;
+  document.getElementById('res17').innerHTML = contactaffect;
+  var infectedcountry = document.querySelector('input[name=touchy]:checked').value;
+  document.getElementById('res18').innerHTML = infectedcountry;
+  var familycontact = document.querySelector('input[name=goodreason]:checked').value;
+  document.getElementById('res19').innerHTML = familycontact;
+  var healthcondition = document.querySelector('input[name=worthwhile]:checked').value;
+  document.getElementById('res20').innerHTML = healthcondition;
+  var diseasesymptoms = document.querySelector('input[name=winddown]:checked').value;
+  document.getElementById('res21').innerHTML = diseasesymptoms;
+  var description = document.getElementById('Description').value;
+  document.getElementById('res22').innerHTML = description;
+  var artifact = document.getElementById('Artifacts').value;
+  document.getElementById('res23').innerHTML = artifact;
+  var test = document.querySelector('input[name=swallowing]:checked').value;
+  document.getElementById('res24').innerHTML = test;
+  var age = document.querySelector('input[name=enjoyment]:checked').value;
+  document.getElementById('res25').innerHTML = age;
+  var gender = document.querySelector('input[name=physicalexertion]:checked').value;
+  document.getElementById('res26').innerHTML = gender;
+  var fever = document.querySelector('input[name=feltdown]:checked').value;
+  document.getElementById('res27').innerHTML = fever;
+  var drycough = document.querySelector('input[name=irritable]:checked').value;
+  document.getElementById('res28').innerHTML = drycough;
+  var feelingsmell = document.querySelector('input[name=closepanic]:checked').value;
+  document.getElementById('res29').innerHTML = feelingsmell;
+  var throatpain = document.querySelector('input[name=somethingupset]:checked').value;
+  document.getElementById('res30').innerHTML = throatpain;
+  var bodyweak = document.querySelector('input[name=trivial]:checked').value;
+  document.getElementById('res31').innerHTML = bodyweak;
+  var nohunger = document.querySelector('input[name=enthusiastic]:checked').value;
+  document.getElementById('res32').innerHTML = nohunger;
+  var coughrange = document.querySelector('input[name=interruptions]:checked').value;
+  document.getElementById('res33').innerHTML = coughrange;
+  var shortness = document.querySelector('input[name=nervoustension]:checked').value;
+  document.getElementById('res34').innerHTML = shortness;
+  var breathing = document.querySelector('input[name=prettyworthless]:checked').value;
+  document.getElementById('res35').innerHTML = breathing;
+  var drowsy = document.querySelector('input[name=intolerant]:checked').value;
+  document.getElementById('res36').innerHTML = drowsy;
+  var chestpain = document.querySelector('input[name=terrified]:checked').value;
+  document.getElementById('res37').innerHTML = chestpain;
+  var weaknessbody = document.querySelector('input[name=hopefulabout]:checked').value;
+  document.getElementById('res38').innerHTML = weaknessbody;
+  var visitedoutside = document.querySelector('input[name=meaningless]:checked').value;
+  document.getElementById('res39').innerHTML = visitedoutside;
+  var contactaffect = document.querySelector('input[name=agitated]:checked').value;
+  document.getElementById('res40').innerHTML = contactaffect;
+  var infectedcountry = document.querySelector('input[name=panic]:checked').value;
+  document.getElementById('res41').innerHTML = infectedcountry;
+  var familycontact = document.querySelector('input[name=trembling]:checked').value;
+  document.getElementById('res42').innerHTML = familycontact;
+  var healthcondition = document.querySelector('input[name=difficultwork]:checked').value;
+  document.getElementById('res43').innerHTML = healthcondition;
+}
+
+
+  function numb(){
+    var sum=0;
+        var a=document.getElementById("quitetrivial");
+        var a2=document.getElementById("quitetrivials");
+        var b=document.getElementById("dryness"); 
+        var b2=document.getElementById("drynesss");   
+        var c=document.getElementById("positivefeeling");
+        var c2=document.getElementById("positivefeelings");
+        var d=document.getElementById("breathing");
+        var d2=document.getElementById("breathings");
+        var e=document.getElementById("getgoing");
+        var e2=document.getElementById("getgoings");
+        var f=document.getElementById("situations");
+        var f2=document.getElementById("situationss");
+        var g=document.getElementById("shakiness");
+        var g2=document.getElementById("shakinesss");
+        var h=document.getElementById("relax");
+        var h2=document.getElementById("relaxs");   
+        var i=document.getElementById("anxious");
+        var i2=document.getElementById("anxiouss");
+        var j=document.getElementById("forward");
+        var j2=document.getElementById("forwards");
+        var k=document.getElementById("upset");
+        var k2=document.getElementById("upsets");
+        var l=document.getElementById("nervous");
+        var l2=document.getElementById("nervouss");
+        var m=document.getElementById("saddepressed");
+        var m2=document.getElementById("saddepresseds");
+        var n=document.getElementById("impatient");
+        var n2=document.getElementById("impatients");   
+        var o=document.getElementById("faintness");
+        var o2=document.getElementById("faintnesss");
+        var p=document.getElementById("lostinterest");
+        var p2=document.getElementById("lostinterests");
+        var q=document.getElementById("worthperson");
+        var q2=document.getElementById("worthpersons");
+        var r=document.getElementById("touchy");
+        var r2=document.getElementById("touchys");
+        var s=document.getElementById("noticeably");
+        var s2=document.getElementById("noticeablys");
+        var t=document.getElementById("goodreason");
+        var t2=document.getElementById("goodreasons");   
+        var u=document.getElementById("worthwhile");
+        var u2=document.getElementById("worthwhiles");
+        var v=document.getElementById("winddown");
+        var v2=document.getElementById("winddowns");
+        var w=document.getElementById("swallowing");
+        var w2=document.getElementById("swallowings");
+        var x=document.getElementById("enjoyment");
+        var x2=document.getElementById("enjoyments");
+        var y=document.getElementById("physicalexertion"); 
+        var y2=document.getElementById("physicalexertions");
+        var z=document.getElementById("feltdown");  
+        var z2=document.getElementById("feltdowns"); 
+        var a1=document.getElementById("irritable");
+        var b1=document.getElementById("closepanic");
+        var c1=document.getElementById("somethingupset");
+        var d1=document.getElementById("trivial");
+        var e1=document.getElementById("enthusiastic");
+        var f1=document.getElementById("interruptions");   
+        var g1=document.getElementById("nervoustension");
+        var h1=document.getElementById("prettyworthless");
+        var i1=document.getElementById("intolerant");
+        var j1=document.getElementById("terrified");
+        var k1=document.getElementById("hopefulabout");
+        var l1=document.getElementById("meaningless");   
+        var m1=document.getElementById("agitated");
+        var n1=document.getElementById("panic");
+        var o1=document.getElementById("trembling");
+        var p1=document.getElementById("difficultwork");
+        var a3=document.getElementById("irritables");
+        var b3=document.getElementById("closepanics");
+        var c3=document.getElementById("somethingupsets");
+        var d3=document.getElementById("trivials");
+        var e3=document.getElementById("enthusiastics");
+        var f3=document.getElementById("interruptionss");   
+        var g3=document.getElementById("nervoustensions");
+        var h3=document.getElementById("prettyworthlesss");
+        var i3=document.getElementById("intolerants");
+        var j3=document.getElementById("terrifieds");
+        var k3=document.getElementById("hopefulabouts");
+        var l3=document.getElementById("meaninglesss");   
+        var m3=document.getElementById("agitateds");
+        var n3=document.getElementById("panics");
+        var o3=document.getElementById("tremblings");
+        var p3=document.getElementById("difficultworks");
+    
+        if(a.checked==true)
+           {
+             sum=sum+1; 
+           } 
+        if(a2.checked==true)
+          {
+           sum=sum+1; 
+          }
+      if(b.checked==true)
+         {
+           sum=sum+1;     
+          } 
+       if(b2.checked==true)
+         {
+           sum=sum+1; 
+          }
+     if(c.checked==true)
+          {
+           sum=sum+1;           
+          } 
+      if(c2.checked==true)
+         {
+           sum=sum+1; 
+          }
+     if(d.checked==true)
+          {
+           sum=sum+1;   
+          } 
+     if(d2.checked==true)
+         {
+           sum=sum+1; 
+          }
+    if(e.checked==true)
+         {
+           sum=sum+1;    
+         } 
+    if(e2.checked==true)
+         {
+           sum=sum+1; 
+          }
+   if(f.checked==true)
+         {
+           sum=sum+1;    
+         } 
+  if(f2.checked==true)
+         {
+           sum=sum+1; 
+          }
+   if(g.checked==true)
+         {
+           sum=sum+1;    
+         } 
+  if(g2.checked==true)
+         {
+           sum=sum+1; 
+          }
+   if(h.checked==true)
+        {
+          sum=sum+1;     
+        } 
+   if(h2.checked==true)
+         {
+           sum=sum+1; 
+          }
+     if(i.checked==true)
+         {
+         sum=sum+1;       
+          } 
+    if(i2.checked==true)
+         {
+           sum=sum+1; 
+          }
+    if(j.checked==true)
+        {
+           sum=sum+1;  
+        }
+    if(j2.checked==true)
+         {
+           sum=sum+1; 
+          }
+    if(k.checked==true)
+        {
+           sum=sum+1; 
+        } 
+  if(k2.checked==true)
+         {
+           sum=sum+1; 
+          }
+    if(l.checked==true)
+        {
+           sum=sum+1;
+        } 
+    if(l2.checked==true)
+         {
+           sum=sum+1; 
+          }
+    if(m.checked==true)
+        {
+           sum=sum+1;  
+        } 
+ if(m2.checked==true)
+         {
+           sum=sum+1; 
+          }
+ if(n.checked==true)
+        {
+           sum=sum+1;
+        } 
+   if(n2.checked==true)
+         {
+           sum=sum+1; 
+          }
+  if(o.checked==true)
+        {
+           sum=sum+1;   
+        } 
+  if(o2.checked==true)
+      {
+           sum=sum+1; 
+       }
+ if(p.checked==true)
+        {
+           sum=sum+1;    
+        } 
+  if(p2.checked==true)
+      {
+           sum=sum+1; 
+       }
+   if(q.checked==true)
+        {
+           sum=sum+1;    
+        } 
+ if(q2.checked==true)
+      {
+           sum=sum+1; 
+       }
+   if(r.checked==true)
+        {
+           sum=sum+1; 
+        } 
+  if(r2.checked==true)
+         {
+           sum=sum+1; 
+         }
+  if(s.checked==true)
+        {
+           sum=sum+1;  
+        } 
+  if(s2.checked==true)
+      {
+           sum=sum+1; 
+       }
+  if(t.checked==true)
+        {
+           sum=sum+1; 
+        }   
+ if(t2.checked==true)
+          {
+           sum=sum+1; 
+          }
+ if(u.checked==true)
+        {
+           sum=sum+1; 
+         }  
+    if(u2.checked==true)
+          {
+           sum=sum+1; 
+       }
+     if(v.checked==true)
+        {
+           sum=sum+1;  
+        }  
+     if(v2.checked==true)
+        {
+           sum=sum+1; 
+       }
+     if(w.checked==true)
+        {
+           sum=sum+1; 
+        }   
+  if(w2.checked==true)
+         {
+           sum=sum+1; 
+         }
+    if(x.checked==true)
+        {
+           sum=sum+1;   
+        } 
+    if(x2.checked==true)
+          {
+           sum=sum+1; 
+         }
+      if(y.checked==true)
+        {
+           sum=sum+1;   
+        } 
+      if(y2.checked==true)
+         {
+           sum=sum+1; 
+          }
+       if(z.checked==true)
+        {
+           sum=sum+1;  
+        }  
+      if(z2.checked==true)
+         {
+           sum=sum+1; 
+         }
+         if(a1.checked==true)
+        {
+           sum=sum+1; 
+        }   
+    if(a3.checked==true)
+          {
+           sum=sum+1; 
+          }
+      if(b1.checked==true)
+          {
+           sum=sum+1; 
+        } 
+    if(b3.checked==true)
+         {
+           sum=sum+1; 
+         } 
+      if(c1.checked==true)
+        {
+           sum=sum+1;  
+        } 
+    if(c3.checked==true)
+          {
+           sum=sum+1; 
+         } 
+    if(d1.checked==true)
+        {
+           sum=sum+1;
+        } 
+  if(d3.checked==true)
+      {
+           sum=sum+1; 
+       } 
+  if(e1.checked==true)
+        {
+           sum=sum+1;  
+        } 
+    if(e3.checked==true)
+        {
+           sum=sum+1; 
+        } 
+    if(f1.checked==true)
+        {
+           sum=sum+1; 
+        }
+    if(f3.checked==true)
+         {
+           sum=sum+1; 
+          }
+   if(g1.checked==true)
+        {
+           sum=sum+1;  
+        } 
+  if(g3.checked==true)
+         {
+           sum=sum+1; 
+          }  
+  if(h1.checked==true)
+        {
+           sum=sum+1;  
+        } 
+    if(h3.checked==true)
+         {
+           sum=sum+1; 
+          }
+   if(i1.checked==true)
+        {
+           sum=sum+1;    
+        } 
+    if(i3.checked==true)
+         {
+           sum=sum+1; 
+          }  
+  if(j1.checked==true)
+        {
+           sum=sum+1; 
+        } 
+    if(j3.checked==true)
+        {
+           sum=sum+1; 
+       } 
+    if(k1.checked==true)
+        {
+           sum=sum+1;
+        } 
+   if(k3.checked==true)
+       {
+           sum=sum+1; 
+       } 
+   if(l1.checked==true)
+        {
+           sum=sum+1;
+        } 
+    if(l3.checked==true)
+      {
+           sum=sum+1; 
+       } 
+   if(m1.checked==true)
+        {
+           sum=sum+1; 
+    } 
+    if(m3.checked==true)
+        {
+            sum=sum+1; 
+       } 
+   if(n1.checked==true)
+        {
+           sum=sum+1;
+        } 
+    if(n3.checked==true)
+         {
+           sum=sum+1; 
+         } 
+         if(o1.checked==true)
+        {
+           sum=sum+1;
+           
+        } 
+   if(o3.checked==true)
+      {
+           sum=sum+1; 
+       } 
+    if(p1.checked==true)
+        {
+           sum=sum+1; 
+        } 
+  if(p3.checked==true)
+      {
+           sum=sum+1; 
+       }
+     
+if(sum<20)
+{
+    document.getElementById("wlow").value="Low";
+}
+else if(sum>20 && sum<30)
+{
+   document.getElementById("wlow").value="Medium";
+}
+else if(sum>30)
+{ 
+  document.getElementById("wlow").value="High";
+}
+
+
+  }
